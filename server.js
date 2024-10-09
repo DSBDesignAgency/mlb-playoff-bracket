40d51cabb1b8fff17b81858aa96f75e5ba5ad2de
David Bradford <59341428+DSBDesignAgency@users.noreply.github.com>
2024-10-09 16:12:04 -0400

    Update NL DS G3 Standings Mets/Phillies Padres/Dodgers

--- server.js
+++ server.js
@@ -61,12 +61,12 @@ const bracketData = {
         ],
 
         division3: [
-            { team: '1-1', logo: 'images/NL/Mets.png', seed: 6 },
-            { team: '1-1', logo: 'images/NL/Phillies.png', seed: 2 },
+            { team: '2-1', logo: 'images/NL/Mets.png', seed: 6 },
+            { team: '1-2', logo: 'images/NL/Phillies.png', seed: 2 },
         ],
         division4: [
-            { team: '1-1', logo: 'images/NL/Padres.png', seed: 4 },
-            { team: '1-1', logo: 'images/NL/Dodgers.png', seed: 1 },
+            { team: '2-1', logo: 'images/NL/Padres.png', seed: 4 },
+            { team: '1-2', logo: 'images/NL/Dodgers.png', seed: 1 },
         ],
         champions3: [
             { team: '0-0', logo: 'images/Default.png', seed: null },
